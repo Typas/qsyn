9166993cde9098262b1077ef2484fc0b05fb369e
ric2k1 <ric2k1@users.noreply.github.com>
2026-03-11 21:09:21 +0800

    Fix linting errors/warnings

--- src/util/boolean_matrix.cpp
+++ src/util/boolean_matrix.cpp
@@ -313,8 +313,8 @@ size_t BooleanMatrix::filter_duplicate_row_operations() {
     for (size_t ith_row_op = 0; ith_row_op < _row_operations.size(); ith_row_op++) {
         auto& [row_src, row_dest] = _row_operations[ith_row_op];
         auto const first_match    = last_used.contains(row_src) &&
-                                 last_used[row_src].row_idx == row_dest &&
-                                 _row_operations[last_used[row_src].op_idx].first == row_src;  // make sure the destinations are matched
+                                    last_used[row_src].row_idx == row_dest &&
+                                    _row_operations[last_used[row_src].op_idx].first == row_src;  // make sure the destinations are matched
 
         auto const second_match = last_used.contains(row_dest) &&
                                   last_used[row_dest].row_idx == row_src &&
